ede3f013d64348e5a199d8cd861b96eca0da725a
GlobalCreativeApkDev <globalcreativeapkdev2022@gmail.com>
2024-03-17 01:21:16 +1300

    Updated entry points

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 
 setup(
     name='gemini-pro-rpg',
-    version='1.2',
+    version='1.3',
     packages=['gemini-pro-rpg'],
     url='https://github.com/GlobalCreativeApkDev/gemini-pro-rpg',
     license='MIT',
@@ -30,7 +30,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "main=main:main",
+            "gemini-pro-rpg=main:main",
         ]
     }
 )
